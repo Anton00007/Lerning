dd93f7e214ef3eadb6e23e7c8fd38cb61ba1a0bf
Anton00007 <toni.golub.07@mail.ru>
2024-12-20 19:54:51 +0600

    Казино2
    Добавил то, что теперь если ввести неверную ставку будет показывать только один раз баланс (до была ошибка, баланс выдавался два раза).
     Сделал так, чтобы если ввести неверное значение в рандомные числа, чтобы возвращало к введению и выдавало ошибку

---
+++ Казино2/Казино2/Program.cs
@@ -0,0 +1,72 @@
+﻿int balance = 1000;
+Console.Write("balance: ");
+Console.WriteLine(balance);
+Random rand = new Random();
+int bet1 = 0;
+bool B = true;
+bool C = true;
+
+while (balance > 0 && B)
+{
+    while (balance > 0 && B)
+    {
+        C = true;
+        Console.WriteLine("Input bet:");
+        string bet = Console.ReadLine();
+        if (bet == "exit")
+        {
+            B = false;
+            C = false;
+        }
+        else
+        {
+            bet1 = Convert.ToInt32(bet);
+        }
+        while (C)
+        {
+            if (bet1 > balance)
+            {
+                Console.WriteLine("You don't have enough funds.");
+                Console.WriteLine("Your balance: "); Console.WriteLine(balance);
+                C = false;
+            }
+            else
+            {
+                while (C)
+                {
+                    Console.WriteLine("Input your number: 0 or 1");
+                    string num1 = Console.ReadLine();
+                    if (num1 == "0" || num1 == "1")
+                    {
+
+                        int num = Convert.ToInt32(num1);
+                        int randomValue = rand.Next(0, 2); // Сгенерировать рандомное число
+                        Console.WriteLine(randomValue);
+                        if (num == randomValue)
+                        {
+                            balance = balance + bet1;
+                            Console.WriteLine("You win");
+
+                        }
+                        else
+                        {
+                            balance = balance - bet1;
+                            Console.WriteLine("You lose");
+                        }
+                        C = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("the value entered is incorrect, you can only enter 0 or 1");
+                    }
+                }
+            }
+        }
+        Console.Write("balance: ");
+        Console.WriteLine(balance);
+    }
+
+
+}
+Console.Write("Game over");
+Console.ReadLine();
